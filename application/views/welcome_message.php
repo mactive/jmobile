b99e33fcbd78690d6f3f24959b058146e8330e3b
mactive <mactive@gmail.com>
2011-10-12 18:29:59 +0800

    build a CI
    CI + jquery mobile

---
+++ application/views/welcome_message.php
@@ -0,0 +1,42 @@
+<?php echo  site_url() . "========"; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Welcome to the world 三联动  </title>	
+	<meta charset="utf-8">
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1"> 	
+	<link rel="stylesheet" href="<?=base_url()?>asset/jquery.mobile-1.0rc1.min.css" />
+
+	<script type="text/javascript" src="<?=base_url()?>asset/jquery-1.6.4.min.js"></script>
+	<script type="text/javascript" src="<?=base_url()?>asset/jquery.mobile-1.0rc1.min.js"></script>
+	
+	
+</head>
+<body>
+dddddd
+<div data-role="page">
+
+	<div data-role="header">
+		<h1>Page Title</h1>
+	</div><!-- /header -->
+
+	<div data-role="content">	
+		<p>Page content goes here.</p>	
+		<p>
+			<a href="page-template.html" data-inline="true" data-theme="b" data-role="button" class="ui-btn ui-btn-inline ui-btn-corner-all ui-shadow ui-btn-down-b ui-btn-hover-b"><span class="ui-btn-inner ui-btn-corner-all"><span class="ui-btn-text">View boilerplate template</span></span></a>
+		</p>	
+		
+		<p>
+			<a href="../../docs/pages/multipage-template.html" data-inline="true" data-theme="b" data-role="button" rel="external" class="ui-btn ui-btn-inline ui-btn-corner-all ui-shadow ui-btn-up-b"><span class="ui-btn-inner ui-btn-corner-all"><span class="ui-btn-text">View multi-page template</span></span></a>
+		</p>
+	</div><!-- /content -->
+
+	<div data-role="footer">
+		<h4>Page Footer</h4>
+	</div><!-- /footer -->
+</div><!-- /page -->
+
+</body>
+</html>
+	
